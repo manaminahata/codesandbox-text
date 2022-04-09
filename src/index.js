6f7b6314c50bbb6595675dd35bbac7ddec4b6ce4
manaminahata <93502848+manaminahata@users.noreply.github.com>
2022-04-09 14:57:34 +0900

    constの学習

--- src/index.js
+++ src/index.js
@@ -21,3 +21,12 @@
 
 // // let変数は再宣言不可能
 // let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+// const変数は上書き不可
+// val3 = "const変数を上書き";
+
+// const変数は再宣言不可
+// const val3 = "const変数の再宣言";
