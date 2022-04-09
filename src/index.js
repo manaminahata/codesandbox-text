350e49829e17a1b253ad49d5270335270a1b6a25
manaminahata <93502848+manaminahata@users.noreply.github.com>
2022-04-09 15:17:47 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@
 // // let変数は再宣言不可能
 // let val2 = "let変数を再宣言";
 
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
 // const変数は上書き不可
 // val3 = "const変数を上書き";
@@ -31,10 +31,19 @@ console.log(val3);
 // const変数は再宣言不可
 // const val3 = "const変数の再宣言";
 
-const val4 = {
-  name: "manami",
-  age: 24
-};
-val4.name = "mana";
-val4.address = "yokohama";
-console.log(val4);
+// const val4 = {
+//   name: "manami",
+//   age: 24
+// };
+// val4.name = "mana";
+// val4.address = "yokohama";
+// console.log(val4);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "manami";
+const age = 24;
+// 「私の名前はmanamiです。年齢は24歳です。」
+console.log(`私の名前は${name}です。年齢は${age}歳です。`);
