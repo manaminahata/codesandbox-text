febb535141603b6b25b04f4187e8b59b563e5198
manaminahata <93502848+manaminahata@users.noreply.github.com>
2022-04-10 16:59:28 +0900

    デフォルト値の学習

--- src/index.js
+++ src/index.js
@@ -77,11 +77,8 @@
 // const message2 = `名前は${name}です。年齢は${age}です。`;
 // console.log(message2);
 
-const myProfile = ["manami", 24];
-
-const message1 = `私の名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message1);
-
-const [name, age] = myProfile;
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん！`);
+sayHello("manami");
